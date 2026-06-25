67ffd84d3b7aa57d426e53fb7ef96ad911c0324c
codpangestu <arvstu.id@gmail.com>
2026-06-26 01:12:33 +0700

    feat(route): refresh code

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/migrate-seed', function () {
+    try {
+        \Illuminate\Support\Facades\Artisan::call('migrate:fresh', ['--seed' => true, '--force' => true]);
+        return 'Migration and seeding finished successfully!';
+    } catch (\Exception $e) {
+        return 'Error: ' . $e->getMessage();
+    }
+});
